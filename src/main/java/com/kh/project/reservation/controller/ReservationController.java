b07c64e70f09e115c2026d72aa006fbff28c6cf1
YJH <YJH@DESKTOP-QAKI2DC>
2021-10-28 15:09:20 +0900

    2

--- src/main/java/com/kh/project/reservation/controller/ReservationController.java
+++ src/main/java/com/kh/project/reservation/controller/ReservationController.java
@@ -16,7 +16,7 @@ public class ReservationController {
 	@Resource(name = "reservationService")
 	private ReservationService reservationService;
 	
-	//1. 회원가입 페이지 가기
+	//
 	@GetMapping("/screeningSchedule")
 	public String screeningSchedule() {
 		return "reservation/screening_schedule";
