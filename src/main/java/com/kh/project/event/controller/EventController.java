1b1d491ad31e7cd130be50dcff660ee5369d1d90
alud0 <alud0@DESKTOP-V1EVOKQ>
2021-11-02 11:28:04 +0900

    기량; 메뉴수정/이벤트; 컨트롤러,메인페이지

--- src/main/java/com/kh/project/event/controller/EventController.java
+++ src/main/java/com/kh/project/event/controller/EventController.java
@@ -1,12 +1,26 @@
 package com.kh.project.event.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.kh.project.event.service.EventService;
 
 @Controller
 @RequestMapping("/event")
 public class EventController {
 	//이벤트 관련 기능
 	
+	@Resource(name = "eventService")
+	private EventService eventService;
 	
+	// 이벤트 페이지로 이동
+	@GetMapping("/eventMain")
+	public String eventMain() {
+		return "event/eventMain";
+		
+	}
 }
